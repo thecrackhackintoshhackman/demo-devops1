41ef3b85eb86eac34a9fe70f3b8195d602555710
thecrackhackintoshhackman <hackintoshhackman1@gmail.com>
2025-12-08 11:44:00 +0100

    back

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,29 +9,16 @@ terraform {
 
 provider "docker" {}
 
-# Variable pour l'image Docker (dynamiquement depuis GitHub Actions)
-variable "docker_image" {
-  description = "Docker image to deploy"
-  type        = string
-  default     = "hackintoshhackman/mon-site:latest"
+resource "docker_image" "mon_site" {
+  name = "hackintoshhackman/mon-site:latest"
 }
 
-# Docker container
 resource "docker_container" "mon_site" {
   name  = "mon-site-container"
-  image = var.docker_image
+  image = docker_image.mon_site.name
 
-  # Ports
   ports {
     internal = 80
     external = 8080
   }
-
-  # Optionnel : volume pour voir les changements locaux (utile en dev local)
-  # volumes {
-  #   host_path      = "../site"  # chemin vers ton index.html
-  #   container_path = "/usr/share/nginx/html"
-  # }
-
-  restart = "no"
 }
